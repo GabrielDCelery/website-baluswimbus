57e491038a9cb351c47e19194e332bc8c76618ad
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2015-12-22 11:32:48 +0100

    Finished the basic layout for the website, the arrangements of the elements and the interactive navbar (content section is still not added)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('_main.js is running...');
+(function(){
+
+$(document).ready(function(){
+	console.log('jquery is running...');	
+
+/********************************************************************
+VARIABLES
+********************************************************************/
+	var main = $('main');
+	var nav = $('nav')
+	var navCollapse = $('#nav-collapse');
+	var navCollapseImages = $('#nav-collapse img');
+
+/********************************************************************
+EVENT BINDERS
+********************************************************************/
+
+	navCollapse.on('click', toggleMenu);
+
+/********************************************************************
+FUNCTIONS
+********************************************************************/
+
+	function toggleMenu(){
+		nav.toggleClass('navCollapse');
+		main.toggleClass('mainExtend');
+		navCollapseImages.toggle();
+	}
+
+});
+
+}());
